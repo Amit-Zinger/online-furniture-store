13b6a62319d45f5509c4a41a1c55d98c3fe17af8
Elad Shneor <elad6013@gmail.com>
2025-02-09 15:28:35 +0200

    Replace factory class file
    changing furniture factory class for more straight forward implantation

--- models/factory.py
+++ models/factory.py
@@ -1,96 +1,41 @@
-from furniture import Furniture
-
+from furniture import Chair, Sofa, Table, Bed, Closet
 
 class FurnitureFactory:
     @staticmethod
-    def create_furniture(furniture_type, **kwargs):
+    def create_furniture(furniture_desc):
         """
         Factory method to create furniture objects.
 
         Parameters:
-        furniture_type: Type of furniture (e.g., 'Chair', 'Sofa').
-        kwargs: Attributes required to create the furniture object.
+        furniture_desc: Dictionary containing furniture attributes.
 
         Outputs:
         Instance of the specified furniture class.
         """
-        if not (kwargs.get('serial_number') and kwargs.get('name') and kwargs.get('price')):
-            raise ValueError("Basic attributes missing fail to create furniture object")
+        if not (furniture_desc.get('serial_number') and furniture_desc.get('name') and furniture_desc.get('price')):
+            raise ValueError("Basic attributes missing, failed to create furniture object")
 
-        # Creating Chair object
+        furniture_classes = {
+            "Chair": Chair,
+            "Sofa": Sofa,
+            "Table": Table,
+            "Bed": Bed,
+            "Closet": Closet
+        }
+        furniture_type = furniture_desc.pop('type')
+
+        if not furniture_type in furniture_classes:
+            raise ValueError(f"Unknown furniture type: {furniture_type}")
+
         if furniture_type == 'Chair':
-            return Chair(
-                name=kwargs.get('name', 'Unnamed'),
-                description=kwargs.get('description', ''),
-                price=kwargs.get('price', 0),
-                dimensions=kwargs.get('dimensions', 'Unknown'),
-                serial_number=kwargs.get('serial_number', 'Unknown'),
-                quantity=kwargs.get('quantity', 0),
-                weight=kwargs.get('weight', 0),
-                manufacturing_country=kwargs.get('manufacturing_country', 'Unknown'),
-                # Specific attributes for chair
-                has_weels=kwargs.get('has_weels', False),
-                how_many_legs=kwargs.get('how_many_legs', 'Unknown')
-            )
-        # Creating Sofa object
+            return Chair(**furniture_desc)
         elif furniture_type == 'Sofa':
-            return Sofa(
-                name=kwargs.get('name', 'Unnamed'),
-                description=kwargs.get('description', ''),
-                price=kwargs.get('price', 0),
-                dimensions=kwargs.get('dimensions', 'Unknown'),
-                serial_number=kwargs.get('serial_number', 'Unknown'),
-                quantity=kwargs.get('quantity', 0),
-                weight=kwargs.get('weight', 0),
-                manufacturing_country=kwargs.get('manufacturing_country', 'Unknown'),
-                # Specific attributes for sofa
-                can_turn_to_bad=kwargs.get('can_turn_to_bad', False),
-                how_many_seats=kwargs.get('how_many_seats', 'Unknown')
-            )
-        # Creating Table object
+            return Sofa(**furniture_desc)
         elif furniture_type == 'Table':
-            return Table(
-                name=kwargs.get('name', 'Unnamed'),
-                description=kwargs.get('description', ''),
-                price=kwargs.get('price', 0),
-                dimensions=kwargs.get('dimensions', 'Unknown'),
-                serial_number=kwargs.get('serial_number', 'Unknown'),
-                quantity=kwargs.get('quantity', 0),
-                weight=kwargs.get('weight', 0),
-                manufacturing_country=kwargs.get('manufacturing_country', 'Unknown'),
-                # Specific attributes for table
-                expandable=kwargs.get('expandable', False),
-                how_many_seats=kwargs.get('how_many_seats', 'Unknown')
-            )
-        # Creating Bed object
+            return Table(**furniture_desc)
         elif furniture_type == 'Bed':
-            return Bed(
-                name=kwargs.get('name', 'Unnamed'),
-                description=kwargs.get('description', ''),
-                price=kwargs.get('price', 0),
-                dimensions=kwargs.get('dimensions', 'Unknown'),
-                serial_number=kwargs.get('serial_number', 'Unknown'),
-                quantity=kwargs.get('quantity', 0),
-                weight=kwargs.get('weight', 0),
-                manufacturing_country=kwargs.get('manufacturing_country', 'Unknown'),
-                # Specific attributes for bad
-                has_storage=kwargs.get('has_storage', False),
-                has_back=kwargs.get('has_back', False)
-            )
-        # Creating Closet object
+            return Bed(**furniture_desc)
         elif furniture_type == 'Closet':
-            return Closet(
-                name=kwargs.get('name', 'Unnamed'),
-                description=kwargs.get('description', ''),
-                price=kwargs.get('price', 0),
-                dimensions=kwargs.get('dimensions', 'Unknown'),
-                serial_number=kwargs.get('serial_number', 'Unknown'),
-                quantity=kwargs.get('quantity', 0),
-                weight=kwargs.get('weight', 0),
-                manufacturing_country=kwargs.get('manufacturing_country', 'Unknown'),
-                # Specific attributes for closet
-                has_drawers=kwargs.get('has_drawers', False),
-                how_many_doors=kwargs.get('how_many_doors', 'Unknown')
-            )
+            return Closet(**furniture_desc)
         else:
             raise ValueError(f"Unknown furniture type: {furniture_type}")
