ecbf484c4291a9cb7391ab19fe4bec9932508c9a
nitzanifrah <nitzanifr@gmail.com>
2025-02-18 20:40:52 +0200

    Update cart.py
    editing few parts to match the functionallity of the old shopping cart( improved the purchase proccess-
    missing-
    1. function to validate cart ( i recommend using a function from invertory)
    2. update invertory - based on an invertory function and not in cart.

--- models/cart.py
+++ models/cart.py
@@ -51,7 +51,8 @@ class ShoppingCart:
         """החלת הנחה כללית על העגלה."""
         total = self.calculate_total()
         return calc_discount(total, discount_percentage)
-
+    def validate_cart(self):
+        return #we need to implement this in invertory - get a cart anc check the invertory
     def purchase(self, payment_gateway):
         """סיום הקנייה וניקוי העגלה לאחר תשלום מוצלח."""
         total = self.calculate_total()
@@ -65,3 +66,30 @@ class ShoppingCart:
             print("Purchase successful!")
             return True
         return False
+    def checkout(self, payment_info):
+        print("Starting checkout process...")
+        if not self.validate_cart():
+            print("Checkout failed: Some items are unavailable.")
+            return False
+
+        total_price = calcualte_total(self)
+         
+        if not self.process_payment(payment_info, total_price):
+            print("Checkout failed: Payment processing error.")
+            return False
+
+        print("Payment successful. Creating order...")
+        order_manager = OrderManager()
+        order_manager.create_order(self, payment_info, total_price)
+
+        print("Updating inventory...")
+        self.inventory.update_inventory(self.cart) # We need to add in invertory a function that gets a cart and can update it at once instead of updating 1 item quantity
+
+        self.clear_cart()
+        print("Checkout process completed successfully.")
+        return True
+
+    def process_payment(self, payment_info, total_price):
+        print(f"Processing payment of {total_price} for client {self.client_id}...")
+        return True  # Mock success
+        
